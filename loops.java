5962f089118785a85ce50137c20b88f24f86cd07
Sanjeevani Gandhakshe <98202766+Sanjeevani018@users.noreply.github.com>
2022-11-13 16:21:48 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,15 @@
+/*Loops :(for repeating task) 
+1.while 2. for 3. do while */
+
+import java.util.*;
+public class javabasics{
+    public static void main(String args[]){
+       // Scanner sc = new Scanner(System.in);
+        int counter =0;
+        while(counter < 1000){
+            System.out.println("Sanjeevani ");
+            counter++;
+        }
+        System.out.println("printed");
+    } 
+}
